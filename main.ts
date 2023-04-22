b22b8541edb1133d811c49b3b5262df9acf078c7
s110170 <131328706+s110170@users.noreply.github.com>
2023-04-22 17:06:15 +0800

    變成正計時器

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
-for (let index = 0; index <= 10; index++) {
-    music.playTone(262, music.beat(BeatFraction.Quarter))
-    basic.showNumber(9 - index)
-}
-music.playTone(392, music.beat(BeatFraction.Whole))
-basic.showString("Start!")
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index <= 9; index++) {
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        basic.showNumber(1 + index)
+    }
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    basic.showString("Stop！")
+})
